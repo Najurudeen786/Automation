78e1a38f58b0ddff73059d9f0bc0a6141ba9ce48
najurudeen786 <najurudeenv@gmail.com>
2019-11-09 17:44:00 +0530

    done

--- target/test-classes/org/cts/data/Gmail.java
+++ target/test-classes/org/cts/data/Gmail.java
@@ -1,11 +1,20 @@
 package org.cts.data;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Gmail {
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Poo\\Desktop\\project\\projeect\\driver\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
-	driver.get("https://www.google.com/");
-	}
+	driver.get("https://www.facebook.com/");
+	
+
+	   WebElement ms= driver.findElement(By.xpath("//a[contains(text(),'All Departments')]"));
+	   Actions art=new Actions(driver);
+	   art.moveToElement(ms).perform();
+
+}
 	}
