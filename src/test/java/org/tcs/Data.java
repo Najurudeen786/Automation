6cef1036fbbc6eb43552e1e9a251117c4c5277d5
najurudeen786 <najurudeenv@gmail.com>
2019-11-09 17:15:05 +0530

    nitish

---
+++ src/test/java/org/tcs/Data.java
@@ -0,0 +1,11 @@
+package org.tcs;
+
+import org.openqa.selenium.WebDriver;
+
+public class Data {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Poo\\\\eclipse-workspace\\\\OldPgm\\\\driver\\\\chromedriver.exe");
+	}
+	
+
+}
--- target/test-classes/org/cts/data/Gmail.java
+++ target/test-classes/org/cts/data/Gmail.java
@@ -1,5 +1,11 @@
 package org.cts.data;
 
-public class Gmail {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-}
+public class Gmail {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Poo\\Desktop\\project\\projeect\\driver\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.google.com/");
+	}
+	}
